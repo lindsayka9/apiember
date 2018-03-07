9dd0b0610d6dcfaedb79c38d09c7cfff6aca4de0
lindsayka9 <kat.lindsay.92@gmail.com>
2018-03-06 19:39:57 -0500

    Add editable to post serializer
    Add editable attribute and definition to post serializer for front end
    purposes (displaying button only when correct user is logged in)

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,10 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content
+  attributes :id, :title, :content, :editable
+
+  def editable
+    scope == object.user
+  end
+
   has_many :comments
   belongs_to :user
 end
