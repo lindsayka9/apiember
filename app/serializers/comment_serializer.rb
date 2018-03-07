5f394caf2c9ca483df70b394d772f2ba47dd8a83
lindsayka9 <kat.lindsay.92@gmail.com>
2018-03-07 09:02:43 -0500

    Add editable attribute to post serializer
    Will return true or false for user auth on the front end

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,9 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :content, :post
+  attributes :id, :content, :post, :editable
+
+  def editable
+    scope == object.user
+  end
   # has_one :user
   belongs_to :post
 end
